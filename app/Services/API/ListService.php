f6e9c781156bc00b98678fecc740741a9d7aeb0c
Ritobroto <ritobrotomukherjee1991@gmail.com>
2022-01-19 17:53:57 +0530

    Updated List API data structure

--- app/Services/API/ListService.php
+++ app/Services/API/ListService.php
@@ -11,7 +11,7 @@ namespace App\Services\API;
 use App\Services\API\APIBaseService;
 use App\Traits\GetTableNameTrait;
 use App\Models\AdminMaster;
-use Illuminate\Support\Facades\Schema;
+use App\Models\UploadDataSetting;
 
 /**
  * Description of ListService
@@ -31,10 +31,11 @@ class ListService extends APIBaseService {
     }
 
     private function getDataList(array $inputs): array {
-        $return = [];
+        $return = ["data_type" => "data", "unique" => "","data" => []];
         $table_name = $this->getTableName($inputs['service_id'], $inputs['organization_id']);
         $hasTable = \Schema::Connection(env('DB_CONNECTION'))->hasTable($table_name);
         if (isset($inputs['customer_id']) && $hasTable) {
+            $return['unique'] = $this->getUniqueColumnforData($inputs['service_id']);
             $return['data'] = \DB::table($table_name)
                     ->join("clients", "$table_name.client_id", "=", "clients.id")
                     ->join("customers", "$table_name.customer_id", "=", "customers.id")
@@ -43,13 +44,20 @@ class ListService extends APIBaseService {
                     ->get();
         }
         if(!$hasTable){
-            $return = $this->getImagesDocuments($inputs['service_id'], $inputs['organization_id']);
+            $return = $this->getImagesDocuments($inputs['service_id'], $inputs['organization_id'], $return);
         }
         return $return;
     }
     
-    private function getImagesDocuments(int $service_id, int $organization_id): array {
-        $return = [];
+    private function getUniqueColumnforData(int $service_id): string {
+        $settings = UploadDataSetting::where('service_id', $service_id)->first();
+        if($settings->id){
+            return array_search("unique", $settings->columns);
+        }
+        return "";
+    }
+    
+    private function getImagesDocuments(int $service_id, int $organization_id, array $return): array {
         $admin_master = AdminMaster::with('admin_master_image')
                 ->where('organization_id', $organization_id)
                 ->where('service_id', $service_id)
@@ -57,16 +65,21 @@ class ListService extends APIBaseService {
         foreach($admin_master as $v){
             $i = 0;
             if(count($v->admin_master_image) > 0 && $v->file_type == "images"){
+                $return['data_type'] = "images";
                 foreach($v->admin_master_image as $image){
                     $index = $i++;
-                    $return['images'][$index]['name'] = $v->file_name;
-                    $return['images'][$index]['link'] = asset($image->image_link);
+                    $return['data'][$index]['id'] = $image->id;
+                    $return['data'][$index]['name'] = $v->file_name;
+                    $return['data'][$index]['url'] = asset($image->image_link);
                 }
             }
             if(isset($v->file_link) && $v->file_type == "pdf"){
                 $index = $i++;
-                $return['docs'][$index]['name'] = $v->file_name;
-                $return['docs'][$index]['url'] = asset($v->file_link);
+                
+                $return['data_type'] = "docs";
+                $return['data'][$index]['id'] = $v->id;
+                $return['data'][$index]['name'] = $v->file_name;
+                $return['data'][$index]['url'] = asset($v->file_link);
             }
         }
         return $return;
